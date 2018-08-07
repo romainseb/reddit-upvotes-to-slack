68aa783cd151ff5116d0c982c0b1b91c32b4fba9
Sébastien Romain <sromain@tlnd-sromain.home>
2018-08-07 07:20:13 +0200

    update

---
+++ src/utils.js
@@ -0,0 +1,41 @@
+import fs from 'fs';
+import morgan from 'morgan';
+import tracer from 'tracer';
+
+export const log = (() => {
+	const logger = tracer.colorConsole();
+	logger.requestLogger = morgan('dev');
+	return logger;
+})();
+
+export const normalizePort = val => {
+	const port = parseInt(val, 10);
+	if (Number.isNaN(port)) return val;
+	if (port >= 0) return port;
+	return false;
+};
+
+export const delay = time =>
+	new Promise(resolve => {
+		setTimeout(() => {
+			resolve();
+		}, time);
+	});
+
+export const fileExists = async filePath => {
+	let exists = true;
+	try {
+		fs.accessSync(filePath);
+	} catch (err) {
+		if (err.code === 'ENOENT') {
+			exists = false;
+		} else {
+			throw err;
+		}
+	}
+	return exists;
+};
+
+export const getReportFilesDir = () => {
+	console.log(getReportFilesDir);
+};
