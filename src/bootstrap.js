3cfd8005658b0f0488fd8a3a8e49b2c41ba8e48d
Sébastien Romain <sromain@tlnd-sromain.local>
2018-11-13 11:08:13 +0100

    wip

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,33 +1,34 @@
-import bodyParser from 'body-parser';
+import bodyParser from "body-parser";
 
-import { log } from './utils';
-import routes from './routes';
+import { log } from "./utils";
+import routes from "./routes";
 
 export default function bootstrap(app) {
-	app.use(bodyParser.json());
-	app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
 
-	// Routes
-	app.use(routes);
+  // Routes
+  app.use(routes);
 
-	// 404
-	app.use((req, res) => {
-		res.status(404).send({
-			status: 404,
-			message: 'The requested resource was not found',
-		});
-	});
+  // 404
+  app.use((req, res) => {
+    console.log(req);
+    res.status(404).send({
+      status: 404,
+      message: "The requested resource was not found"
+    });
+  });
 
-	// 5xx
-	app.use((err, req, res) => {
-		log.error(err.stack);
-		const message =
-			process.env.NODE_ENV === 'production'
-				? "Something went wrong, we're looking into it..."
-				: err.stack;
-		res.status(500).send({
-			status: 500,
-			message,
-		});
-	});
+  // 5xx
+  app.use((err, req, res) => {
+    log.error(err.stack);
+    const message =
+      process.env.NODE_ENV === "production"
+        ? "Something went wrong, we're looking into it..."
+        : err.stack;
+    res.status(500).send({
+      status: 500,
+      message
+    });
+  });
 }
