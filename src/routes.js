c43871266ac3cca55eea1f56ae89068599522a21
Sébastien Romain <sromain@tlnd-sromain.local>
2018-11-13 11:13:45 +0100

    update ?

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,13 @@
-import express from 'express';
+import express from "express";
 
-import { log } from './utils';
+import { log } from "./utils";
 
-import slackCommandRedditList from './routes/slack-command-reddit-list';
+import slackCommandRedditList from "./routes/slack-command-reddit-list";
 
 const router = new express.Router();
-router.post('/slack/command/reddit-list', slackCommandRedditList);
+router.post("/slack/command/reddit-list", slackCommandRedditList);
+router.post("/slack/actions", (req, res) => {
+  console.log(req.body);
+});
 
 export default router;
